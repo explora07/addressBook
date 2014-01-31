599a42cac67c20b57116560a633c2c2497b70846
explora <explora07@gmail.com>
2014-01-31 02:33:53 +0100

    Create AddressBookItems.java

---
+++ AddressBookItems.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class AddressBookItems {
+	private String name;  
+	private String sex;  
+	private Date birthDate;
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}  
+		  
+}
